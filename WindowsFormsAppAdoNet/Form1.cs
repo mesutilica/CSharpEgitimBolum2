7c491778131f5e42beb377bd8d642a36391785ac
mesutilica <mesut.ilica@gmail.com>
2022-08-18 21:35:48 +0300

    18-08-2022 product list tamam

--- WindowsFormsAppAdoNet/Form1.cs
+++ WindowsFormsAppAdoNet/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsAppAdoNet
         {
             InitializeComponent();
         }
+        ProductDAL productDAL = new ProductDAL(); // Veritabanı işlemlerinin olduğu sınıfı tanımladık
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvUrunler.DataSource = productDAL.GetAll(); // Form ön yüzdeki dgvUrunler nesnesine productDAL içindeki GetAll metodu ile ürünleri yüklettik
+        }
     }
 }
---
+++ WindowsFormsAppAdoNet/ProductDAL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data; // Veritabanı işlemleri için gerekli
+using System.Data.SqlClient; // Adonet kütüphaneleri
+
+namespace WindowsFormsAppAdoNet
+{
+    public class ProductDAL
+    {
+        SqlConnection connection = new SqlConnection(@"server=(localdb)\MSSQLLocalDB; database=UrunYonetimiAdoNet; integrated security=true");// SqlConnection veritabanına bağlanmak için kullandığımız ado net sınıfıdır. Parametre olarak kendisine verilen bilgilerdeki veritabanına bağlanır.
+        void ConnectionKontrol() // void = tek
+        {
+            if (connection.State == ConnectionState.Closed) // Eğer yukarda tanımladığımız veritabanı bağlantısı kapalıysa
+            {
+                connection.Open();// bağlantıyı aç
+            }
+        }
+        public List<Product> GetAll() // Bu metodun geri dönüş değeri List<Product> yani ürün listesidir
+        {
+            ConnectionKontrol(); // metot çalıştığı anda bağlantıyı kontrol et
+            List<Product> urunListesi = new List<Product>(); // geriye döndüreceğimiz List<Product> nesnesini oluşturduk
+
+            SqlCommand command = new SqlCommand("select * from Products", connection); // SqlCommand sql komutlarını çalıştırabilmemizi sağlayan ado net sınıfı. Tırnaklar içerisine sql komutumuzu, sonraki parametrede de bu komutun çalıştırılacağı connection nesnesini belirtiyoruz.
+            SqlDataReader reader = command.ExecuteReader(); // SqlDataReader Sql veri okuyucu sınıfıdır, bu sınıfa üstteki command nesnesini ExecuteReader metoduyla çalıştırmasını söyledik.
+
+            while (reader.Read()) // reader db de okuyacak kayıt bulduğu sürece
+            {
+                Product product = new Product() // Döngü her döndüğünde içi boş yeni ürün oluşturuyoruz
+                {
+                    //Aşağıda veritabanından gelen verilerle ürün bilgilerini dolduruyoruz
+                    Id = Convert.ToInt32(reader["Id"]),
+                    UrunAdi = reader["UrunAdi"].ToString(),
+                    StokMiktari = Convert.ToInt32(reader["StokMiktari"]),
+                    UrunFiyati = Convert.ToDecimal(reader["UrunFiyati"])
+                };
+                urunListesi.Add(product); // İçi doldurulan product nesnesini yukarda oluşturduğumuz products listesine ekliyoruz.
+            }
+            reader.Close(); // Veri okuyucuyu kapat
+            command.Dispose(); // sql komut nesnesini kapat
+            connection.Close(); // veritabanı bağlantısını kapat
+            return urunListesi;
+        }
+
+    }
+}
