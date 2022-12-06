99c5933dbe658a17eb489af8e24313215389a808
mesutilica <mesut.ilica@gmail.com>
2022-12-06 21:35:51 +0300

    6-12-22 ViewResults

--- AspNetFrameworkMVC/Controllers/MVC05ModelValidationController.cs
+++ AspNetFrameworkMVC/Controllers/MVC05ModelValidationController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspNetFrameworkMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,12 @@ namespace AspNetFrameworkMVC.Controllers
 {
     public class MVC05ModelValidationController : Controller
     {
+        static List<Uye> uyeListesi = new List<Uye>()
+         {
+             new Uye() { Id = 1, Ad = "Alp", Soyad = "Arslan", Email ="alp@siteadi.com"  },
+             new Uye() { Id = 2, Ad = "Murat", Soyad = "Yılmaz", Email ="murat@kartipi.com"  },
+             new Uye() { Id = 3, Ad = "Mesut", Soyad = "Ilıca", Email = "mesut@gmail.net" }
+         };
         // GET: MVC05ModelValidation
         public ActionResult Index()
         {
@@ -15,7 +22,53 @@ namespace AspNetFrameworkMVC.Controllers
         }
         public ActionResult UyeListesi()
         {
+            return View(uyeListesi); // Ekrana modeli view içerisinde gönderebiliyoruz
+        }
+        public ActionResult YeniUye()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult YeniUye(Uye uye)
+        {
+            if (ModelState.IsValid) // Eğer modeldeki validasyon kurallarına uyulmuşsa, tersi için !ModelState.IsValid
+            {
+                uye.Id = uyeListesi.Count + 1;
+                uyeListesi.Add(uye);
+                // Parametreyle gelen uye nesnesini burada veritabanına kaydedebiliriz
+                return RedirectToAction("UyeListesi");
+            }
+            return View(uye);
+        }
+        public ActionResult UyeDuzenle(int id)
+        {
+            var uyeBilgileri = uyeListesi.FirstOrDefault(u => u.Id == id);
+            return View(uyeBilgileri);
+        }
+        [HttpPost]
+        public ActionResult UyeDuzenle(int id, Uye uye)
+        {
+            if (ModelState.IsValid)
+            {
+                // burada güncelleme yapılır
+                return RedirectToAction("UyeListesi");
+            }
+            return View(uye);
+        }
+        public ActionResult UyeSil(int id)
+        {
+            var uyeBilgileri = uyeListesi.FirstOrDefault(u => u.Id == id);
+            return View(uyeBilgileri);
+        }
+        [HttpPost]
+        public ActionResult UyeSil(int id, Uye uye)
+        {
+            if (ModelState.IsValid)
+            {
+                // burada silme yapılır
+                return RedirectToAction("UyeListesi");
+            }
+            return View(uye);
         }
     }
 }
--- AspNetFrameworkMVC/Models/Uye.cs
+++ AspNetFrameworkMVC/Models/Uye.cs
@@ -1,18 +1,32 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations; // Validation işlemleri için gerekli kütüphane
 
 namespace AspNetFrameworkMVC.Models
 {
     public class Uye
     {
         public int Id { get; set; }
+        [Required, StringLength(50)]
         public string Ad { get; set; }
+        [Required, StringLength(50)]
         public string Soyad { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        [Phone]
         public string Telefon { get; set; }
+        [Display(Name = "Tc Kimlik Numarası")]
+        [MinLength(11, ErrorMessage = "Tc Kimlik Numarası 11 Karakter Olmalıdır!")]
+        [MaxLength(11, ErrorMessage = "{0} 11 Karakter Olmalıdır!")]
         public string TcKimlikNo { get; set; }
+        [Display(Name = "Kayıt Tarihi")]
         public DateTime KayitTarihi { get; set; }
+        [Display(Name = "Kullanıcı Adı")]
         public string KullaniciAdi { get; set; }
+        [Display(Name = "Şifre"), DataType(DataType.Password)]
+        [Required, StringLength(50, ErrorMessage = "{0} {2} Karakterden Az Olamaz!", MinimumLength = 3)]
         public string Sifre { get; set; }
+        [Display(Name = "Şifre Tekrar"), DataType(DataType.Password)]
+        [Compare("Sifre")]
         public string SifreTekrar { get; set; }
     }
 }
