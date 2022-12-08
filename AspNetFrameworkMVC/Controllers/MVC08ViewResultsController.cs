361703e8ebdf3ea78f0eab48514963948be9cbd4
mesutilica <mesut.ilica@gmail.com>
2022-12-08 21:33:23 +0300

    8-12-2022 cookies mvc

--- AspNetFrameworkMVC/Controllers/MVC08ViewResultsController.cs
+++ AspNetFrameworkMVC/Controllers/MVC08ViewResultsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspNetFrameworkMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -31,7 +32,59 @@ namespace AspNetFrameworkMVC.Controllers
         }
         public RedirectToRouteResult RouteYonlendir()
         {
-            return RedirectToRoute("Default", new { controller = "Home", action = "Index", id=18 });
+            return RedirectToRoute("Default", new { controller = "Home", action = "Index", id = 18 });
+        }
+        public PartialViewResult KategorileriGetirPartial()
+        {
+            //return PartialView("_KategorilerPartial");
+            return PartialView("_PartialLogin");
+        }
+        public PartialViewResult KategorileriModelleGetirPartial()
+        {
+            // Partial view dönerken model verisi kullanabiliriz
+            List<string> kategoriler = new List<string>()
+            {
+                "Bilgisayar", "Monitör", "Klavye", "Telefon", "Laptop"
+            };
+            return PartialView("_KategorilerPartialModelKullanimi", kategoriler);
+        }
+        public JavaScriptResult JsResult()
+        {
+            return JavaScript("alert('JavaScriptResult')");
+        }
+        public JsonResult JsonResult()
+        {
+            var kullanici = new Kullanici()
+            {
+                Ad = "Ali",
+                Soyad = "Çakmaktaş",
+                KullaniciAdi = "acakmak",
+                Email = "ali@cakmaktas.com"
+            };
+            return Json(kullanici, JsonRequestBehavior.AllowGet); // JsonRequestBehavior.AllowGet özelliği bu metoda gelen get isteklerine izin ver demek
+        }
+        public ContentResult XmlContentResult()
+        {
+            var xml = @"
+                <urunler>
+                    <urun>
+                        <Id>1</Id>
+                        <UrunAdi>Klavye</UrunAdi>
+                        <Fiyati>199</Fiyati>
+                        <Stok>3</Stok>
+                        <Kategori>Bilgisayar</Kategori>
+                    </urun>
+                    <urun>
+                        <Id>2</Id>
+                        <UrunAdi>Mouse</UrunAdi>
+                        <Fiyati>99</Fiyati>
+                        <Stok>5</Stok>
+                        <Kategori>Bilgisayar</Kategori>
+                    </urun>
+                </urunler>
+            ";
+
+            return Content(xml, "application/xml"); // geriye xml formatında içerik dön
         }
     }
 }
